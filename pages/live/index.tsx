ceb053f47ea18fa98f4f84294221d96b6b17e3e2
Joie-Kim <joie.huiju@gmail.com>
2023-08-14 14:03:10 +0900

    fix: link error

--- pages/live/index.tsx
+++ pages/live/index.tsx
@@ -8,13 +8,11 @@ const Live: NextPage = () => {
     <Layout hasTabBar title="라이브">
       <div className=" space-y-4 divide-y-[1px]">
         {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-          <Link key={i} href={`/live/${i}`}>
-            <a className="block px-4  pt-4">
-              <div className="aspect-video w-full rounded-md bg-slate-300 shadow-sm" />
-              <h1 className="mt-2 text-2xl font-bold text-gray-900">
-                Galaxy S50
-              </h1>
-            </a>
+          <Link key={i} href={`/live/${i}`} className="block px-4  pt-4">
+            <div className="aspect-video w-full rounded-md bg-slate-300 shadow-sm" />
+            <h1 className="mt-2 text-2xl font-bold text-gray-900">
+              Galaxy S50
+            </h1>
           </Link>
         ))}
         <FloatingButton href="/live/create">
